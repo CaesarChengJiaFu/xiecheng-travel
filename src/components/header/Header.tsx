938d7e5af52d47e7ea365e852580043e013ee1c3
CaesarChengJiaFu <jiafusiyuetian94@163.com>
2022-09-30 14:42:06 +0800

    函数式组件使用react-redux

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,41 +4,67 @@ import styles from './Header.module.css';
 import { Layout, Typography, Input, Menu, Button, Dropdown } from 'antd';
 import { GlobalOutlined } from '@ant-design/icons';
 import { useParams, useLocation, useNavigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from "react-redux";
+// import { useSelctor } from 'react/hooks';// 使用TypeUseSelectorHook这个interface 来重新定义 useSelector这个hook
+// import { RootState } from "../../redux/store";
+import { useSelector } from '../../redux/hooks';
+import { addLanguageActionCreator, changeLanguageActionCreator } from "../../redux/language/languageActions";
 
 export const Header: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const params = useParams();
   const { t } = useTranslation()
+  const language = useSelector(state => state.language );
+  const languageList = useSelector(state => state.languageList );
+  const dispatch = useDispatch();
+
+  // const language = useSelector<RootState>(state => state.language)
+  /* 使用useSelctor这个hook来获取store中的数据，其中一个目的就是解决组件与store数据耦合问题。 如果每次在组	 件中使用useSelector都要传入RootState类型，就讲store与组件深度绑定，就将导致组件不能复用
+  => 所以，需要使用 TypeUseSelectorHook 这个interface，来讲RootStore 这个类型剥离开
+  */
+
+  const menuClickHandler = (e) => {
+    if (e.key === "new") {
+      dispatch(addLanguageActionCreator("新语言", "new_lang"))
+    } else {
+      dispatch(changeLanguageActionCreator(e.key))
+    }
+  };
 
   return (
     <div className={styles['app-header']}>
       <div className={styles['top-header']}>
         <div className={styles.inner}>
-          <Typography.Text>让旅游更幸福</Typography.Text>
+          <Typography.Text>{t("header.slogan")}</Typography.Text>
           <Dropdown.Button
             style={{ marginLeft: 15 }}
             overlay={
               <Menu
+                onClick={menuClickHandler}
                 items={[
-                  { key: "1", label: "中文" },
-                  { key: "2", label: "English" }
-                ]}></Menu>
+                  ...languageList.map((l) => {
+                    return { key: l.code, label: l.name };
+                  }),
+                  { key: "new", label: t("header.add_new_language") },
+                ]}
+              />
             }
-            icon={<GlobalOutlined />}>
-            语言
+            icon={<GlobalOutlined />}
+          >
+            {language === "zh" ? "中文" : "English"}
           </Dropdown.Button>
           <Button.Group className={styles['button-group']}>
-            <Button onClick={() => navigate('/register')}>注册</Button>
-            <Button onClick={() => navigate('/signIn')}>登陆</Button>
+            <Button onClick={() => navigate('/register')}>{t("header.register")}</Button>
+            <Button onClick={() => navigate('/signIn')}>{t("header.signin")}</Button>
           </Button.Group>
         </div>
       </div>
       <Layout.Header className={styles['main-header']}>
         <span onClick={() => navigate('/')}>
           <img src={logo} alt="logo" className={styles['App-logo']} />
-          <Typography.Title level={3} className={styles.title}>React携程旅游网</Typography.Title>
+          <Typography.Title level={3} className={styles.title}>{t("header.title")}</Typography.Title>
         </span>
         <Input.Search
           placeholder="请输入旅游目的地、主题、或关键字"
@@ -47,22 +73,22 @@ export const Header: React.FC = () => {
       </Layout.Header>
       <Menu mode={"horizontal"} className={styles['main-menu']}
         items={[
-          { key: "1", label: "旅游首页" },
-          { key: "2", label: "跟团游" },
-          { key: "3", label: "周末游" },
-          { key: "4", label: "自由行" },
-          { key: "5", label: "私家团" },
-          { key: "6", label: "游轮" },
-          { key: "7", label: "旅游+景点" },
-          { key: "8", label: "当地玩乐" },
-          { key: "9", label: "主题游" },
-          { key: "10", label: "定制游" },
-          { key: "11", label: "游学" },
-          { key: "12", label: "企业游" },
-          { key: "13", label: "高端游" },
-          { key: "14", label: "爱玩户外" },
-          { key: "15", label: "保险" },
-          { key: "16", label: "签证" }
+          { key: "1", label: t("header.home_page") },
+          { key: "2", label: t("header.weekend") },
+          { key: "3", label: t("header.group") },
+          { key: "4", label: t("header.backpack") },
+          { key: "5", label: t("header.private") },
+          { key: "6", label: t("header.cruise") },
+          { key: "7", label: t("header.hotel") },
+          { key: "8", label: t("header.local") },
+          { key: "9", label: t("header.theme") },
+          { key: "10", label: t("header.custom") },
+          { key: "11", label: t("header.study") },
+          { key: "12", label: t("header.visa") },
+          { key: "13", label: t("header.enterprise") },
+          { key: "14", label: t("header.high_end") },
+          { key: "15", label: t("header.outdoor") },
+          { key: "16", label: t("header.insurance") },
         ]}>
       </Menu>
     </div>
