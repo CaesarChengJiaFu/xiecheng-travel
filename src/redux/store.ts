2cbbe496cae233f07a9d2f42655bd6eaa89d8fff
CaesarChengJiaFu <jiafusiyuetian94@163.com>
2022-10-13 15:23:50 +0800

    使用redux-toolkit简化redux样板代码

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,18 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import languageReducer from "./language/languageReducer";
 import recommendProductsReducer from "./recommendProducts/recommendProductsReducer";
 import thunk from "redux-thunk";
+import { actionLog } from "./middlewares/actionLog";
+import { productDetailSlice } from "./productDetail/slice";
+import { combineReducers } from '@reduxjs/toolkit';
 
 const rootReducer = combineReducers({
     languageReducer,
-    recommendProductsReducer
+    recommendProductsReducer,
+    productDetailReducer: productDetailSlice.reducer
 })
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk, actionLog));
 
 //ReturnType可以拿到store.getState的返回值类型
 export type RootState = ReturnType<typeof store.getState>
